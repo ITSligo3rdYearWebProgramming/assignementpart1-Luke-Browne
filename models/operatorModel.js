fca54616c3eb8c97ba700ac9ec779b30be974283
Luke Browne <S00187306@mail.itsligo.ie>
2020-11-22 18:45:55 +0000

    Assignment 2

--- models/operatorModel.js
+++ models/operatorModel.js
@@ -10,11 +10,14 @@ const opSchema = new Schema( // Operator Schema
     name: { type: String }, // Real Name
     opName: { type:String, required:true }, // Code Name
     ctu:  { type: String, required: true }, // Team Name
-    icon: { type:String, required: true },  // Op Icon Image
+    icon: { type:String, required: true }, // Op Icon Image
+    country: { type:String, required:true }, // Country Flag Image
+    side: { type:String, required:true }, // Attack or Defense
+    role: { type:String }                // job on the team
     },         
     {toJSON: {virtuals:true}})
   opSchema.virtual('uri').get(function()  {
-    return `/operators/${this._id}` ; // eg. Jager is a Defender from the GSG9 which is German so - GER-D-1
+    return `/operators/${this._id}` ;
 });
 
 opSchema.plugin(uniqueValidator);
